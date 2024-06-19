582f79890e76ee0cab26170e2eaedf2fdd8c5155
JohnsonBaby123 <binhtt2004@gmail.com>
2024-06-19 23:02:57 +0700

    ttbapi

--- lesson10/src/api/TtbApi.js
+++ lesson10/src/api/TtbApi.js
@@ -1,4 +1,4 @@
 import axios from 'axios'
 export default axios.create({
-    baseURL:"https://666fcdb30900b5f872484285.mockapi.io/tdtApi/tdt/v1/"
+    baseURL:"https://666a665b7013419182cf21b5.mockapi.io/ttbStudent"
 })
